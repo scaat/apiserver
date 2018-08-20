9bdb23b90bbb671141fca93edb359b245410fe50
Scaat Feng <scaat.feng@gmail.com>
2018-08-20 18:29:54 +0800

    feature: add health check and resource status handler.

--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yilingfeng/apiserver/handler/sd"
 	"github.com/yilingfeng/apiserver/router/middleware"
 )
 
@@ -11,7 +12,7 @@ import (
 func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 	// MiddleWares.
 	g.Use(gin.Recovery())
-	g.Use(middleware.NoCache)
+	// g.Use(middleware.NoCache)
 	g.Use(middleware.Options)
 	g.Use(middleware.Secure)
 	g.Use(mw...)
@@ -20,6 +21,14 @@ func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 	g.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "The incorrect API route.")
 	})
+
+	svcd := g.Group("/sd")
+	{
+		svcd.GET("health", sd.HealthCheck)
+		svcd.GET("/disk", sd.DiskCheck)
+		svcd.GET("/cpu", sd.CPUCheck)
+		svcd.GET("/ram", sd.RAMCheck)
+	}
 
 	return g
 }
