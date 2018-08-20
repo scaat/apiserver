bb0abefe56eec8072f01ce177c8e511dc2f881b7
Scaat Feng <scaat.feng@gmail.com>
2018-08-20 20:13:50 +0800

    bugfix: modify router config.

--- router/router.go
+++ router/router.go
@@ -24,7 +24,7 @@ func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 
 	svcd := g.Group("/sd")
 	{
-		svcd.GET("health", sd.HealthCheck)
+		svcd.GET("/health", sd.HealthCheck)
 		svcd.GET("/disk", sd.DiskCheck)
 		svcd.GET("/cpu", sd.CPUCheck)
 		svcd.GET("/ram", sd.RAMCheck)
