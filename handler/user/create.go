3bcbcbf904f14ca985575db95d44d058b8873be3
Scaat Feng <scaat.feng@gmail.com>
2018-08-23 13:47:34 +0800

    feature: add checkParam method to validate CreateRequest.

--- handler/user/create.go
+++ handler/user/create.go
@@ -6,14 +6,20 @@ import (
 	"github.com/yilingfeng/apiserver/pkg/errno"
 	"github.com/lexkong/log"
 	"fmt"
+	"github.com/lexkong/log/lager"
+	"github.com/yilingfeng/apiserver/util"
 )
 
+// Create creates a new user account.
 func Create(c *gin.Context) {
-	var r struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
 	
+	log.Info("User Create function called.", lager.Data{"X-Request-Id": util.GetReqID(c)})
+	
+	// var r struct {
+	// 	Username string `json:"username"`
+	// 	Password string `json:"password"`
+	// }
+	var r CreateRequest
 	// var err error
 	
 	if err := c.Bind(&r); err != nil {
@@ -33,20 +39,24 @@ func Create(c *gin.Context) {
 	
 	log.Debugf("username is: [%s], password is [%s]", r.Username, r.Password)
 	
-	if r.Username == "" {
-		// err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")).Add("This is add message.")
-		// log.Errorf(err, "Get an error")
-		SendResponse(c, errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")), nil)
-		return
-	}
+	// if r.Username == "" {
+	// 	// err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")).Add("This is add message.")
+	// 	// log.Errorf(err, "Get an error")
+	// 	SendResponse(c, errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")), nil)
+	// 	return
+	// }
 	
 	// if errno.IsErrUserNotFound(err) {
 	// 	log.Debug("err type is ErrUserNotFound")
 	// }
 	
-	if r.Password == "" {
-		// err = fmt.Errorf("password is empty")
-		SendResponse(c, fmt.Errorf("password is empty"), nil)
+	// if r.Password == "" {
+	// 	// err = fmt.Errorf("password is empty")
+	// 	SendResponse(c, fmt.Errorf("password is empty"), nil)
+	// 	return
+	// }
+	if err := r.checkParam(); err != nil {
+		SendResponse(c, err, nil)
 		return
 	}
 	
@@ -57,4 +67,17 @@ func Create(c *gin.Context) {
 	// code, message := errno.DecodeErr(err)
 	// c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
 	SendResponse(c, nil, rsp)
+}
+
+func (r *CreateRequest) checkParam() error {
+	
+	if r.Username == "" {
+		return errno.New(errno.ErrValidation, nil).Add("username is empty.")
+	}
+	
+	if r.Password == "" {
+		return errno.New(errno.ErrValidation, nil).Add("password is empty.")
+	}
+	
+	return nil
 }
